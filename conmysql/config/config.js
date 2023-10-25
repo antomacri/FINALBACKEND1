7791a37895ebcbd949dabb85d325068b55a5457f
jemprojects <jemprojectsgit@gmail.com>
2023-10-25 14:21:58 -0300

    querys para insertar datos

--- conmysql/config/config.js
+++ conmysql/config/config.js
@@ -7,7 +7,7 @@ module.exports = {
     DB_HOST: process.env.DB_HOST || 'localhost',
     DB_USER: process.env.DB_USER || 'root',
     DB_PASS: process.env.DB_PASS || 'root',
-    DB_NAME: process.env.DB_NAME || "clase29",
+    DB_NAME: process.env.DB_NAME || "MySqlTrailerFlix",
     DB_PORT: process.env.DB_PORT || "3306",
     dialect:  process.env.DB_DIALECT || "mysql",
 
